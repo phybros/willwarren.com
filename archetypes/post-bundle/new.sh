ed1bee464f8d4361f95fd2b34119b589424f57c0
Will Warren <willdavidwarren@gmail.com>
2023-12-06 11:20:09 -0500

    remove erroneous script

--- archetypes/post-bundle/new.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-TITLE=${1:-}
-
-TITLE_SLUG="$(echo -n "$TITLE" | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' | tr A-Z a-z)"
-DATE="$(date +"%F")"
-SLUG="$DATE-$TITLE_SLUG"
-
-hugo new --kind post-bundle posts/$SLUG
